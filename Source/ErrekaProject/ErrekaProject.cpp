08de1d8edbbcace8f94f0d31ec101ba146a2c357
Eduardo Velez <galeneluna@gmail.com>
2023-01-16 07:59:18 -0600

    inicializado erreka project y motor de videojuegos

---
+++ Source/ErrekaProject/ErrekaProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ErrekaProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ErrekaProject, "ErrekaProject" );
